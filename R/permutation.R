fe0224bb3cbaad994328b59f55b640671c24a86c
santikka <santtu.tikka@jyu.fi>
2024-11-04 12:01:24 +0200

    centralities for pemutation test

--- R/permutation.R
+++ R/permutation.R
@@ -1,31 +1,31 @@
 #' Compare Two Networks from Sequence Data Using Permutation Tests
 #'
-#' This function compares two networks built from sequence data using permutation tests.
-#' The function builds Markov models for two sequence objects , computes the transition
-#' probabilities, and compares them by performing permutation tests. It returns the
-#' differences in transition probabilities, effect sizes, p-values, and confidence intervals.
+#' This function compares two networks built from sequence data using
+#' permutation tests. The function builds Markov models for two sequence
+#' objects, computes the transition probabilities, and compares them by
+#' performing permutation tests. It returns the differences in transition
+#' probabilities, effect sizes, p-values, and confidence intervals.
 #'
 #' @export
 #' @family evaluation
 #' @param x A `tna` object containing sequence data for the first `tna` model.
 #' @param y A `tna` object containing sequence data for the second `tna` model.
-#' @param iter An `integer` gicing the number of permutations to perform.
+#' @param iter An `integer` giving the number of permutations to perform.
 #' The default is 1000.
 #' @param paired A `logical` value. If `TRUE`, perform paired permutation tests;
 #' if `FALSE`, perform unpaired tests. The default is `FALSE`.
-#' @param level A `numeric` value gicing the significance level for the
+#' @param level A `numeric` value giving the significance level for the
 #' permutation tests. The default is 0.05.
-#' @return A `list` containing:
+#' @param measures A `character` vector of centrality measures to test.
+#' See [centralities()] for a list of available centralities.
+#' @param ... Additional arguments passed to [centralities()].
+#' @return A `tna_permutation` object which is a `list` with two elements:
+#' `edges` and `centralities`, both containing the following elements
 #'
-#'   * `edge_statistics` A data frame with edge names, original differences,
-#'     and p-values.
-#'   * `original_weights_x` The edge weight matrix from
-#'     the first `tna` model `x`.
-#'   * `original_weights_y` The edge weight matrix from
-#'     the second `tna` model `y`.
-#'   * `true_diffs` The matrix of differences in weights.
-#'   * `sig_diffs` A matrix showing the significant differences
-#'     based on the permutation test.
+#'   * `stats`\cr A `data.frame` of original differences and p-values for
+#'     each edge or centrality measure
+#'   * `diffs_true`\cr A `matrix` of differences in the data.
+#'   * `diffs_sig` A `matrix` showing the significant differences.
 #'
 #' @examples
 #' model_x <- tna(group_regulation[1:1000,])
@@ -33,7 +33,8 @@
 #' # Small number of iterations for CRAN
 #' permutation_test(model_x, model_y, iter = 50)
 #'
-permutation_test <- function(x, y, iter = 1000, paired = FALSE, level = 0.05) {
+permutation_test <- function(x, y, iter = 1000, paired = FALSE, level = 0.05,
+                             measures = character(0), ...) {
   check_tna_seq(x)
   check_tna_seq(y)
   check_positive(iter)
@@ -42,26 +43,36 @@ permutation_test <- function(x, y, iter = 1000, paired = FALSE, level = 0.05) {
   # TODO check that networks can be compared
   data_x <- x$data
   data_y <- y$data
+  combined_data <- rbind(data_x, data_y)
+  n_data_x <- nrow(data_x)
+  n_combined <- n_data_x + nrow(data_y)
   weights_x <- x$weights
   weights_y <- y$weights
   a <- length(attr(data_x, "alphabet"))
   type <- attr(x, "type")
-  diffs_true <- weights_x - weights_y
-  diffs_true_abs <- abs(diffs_true)
+  n_measures <- length(measures)
+  include_centralities <- n_measures > 0L
+  if (include_centralities) {
+    cent_x <- centralities(x, measures = measures, ...)
+    cent_y <- centralities(y, measures = measures, ...)
+    cent_diffs_true <- as.matrix(cent_x[, -1L] - cent_y[, -1L])
+    cent_diffs_true_abs <- abs(cent_diffs_true)
+  }
+  edge_diffs_true <- weights_x - weights_y
+  edge_diffs_true_abs <- abs(edge_diffs_true)
   edge_names <- expand.grid(
     from = rownames(weights_x),
     to = colnames(weights_y)
   )
   edge_names <- paste0(edge_names$from, " -> ", edge_names$to)
-  combined_data <- rbind(data_x, data_y)
-  n_data_x <- nrow(data_x)
-  n_combined <- n_data_x + nrow(data_y)
   perm_x <- seq_len(n_data_x)
   perm_y <- seq(n_data_x + 1L, n_combined)
   combined_model <- markov_model(combined_data, transitions = TRUE)
   combined_trans <- combined_model$trans
-  diffs_perm <- array(0L, dim = c(iter, a, a))
-  p_values <- matrix(0L, a, a)
+  edge_diffs_perm <- array(0L, dim = c(iter, a, a))
+  cent_diffs_perm <- array(0L, dim = c(iter, a, n_measures))
+  edge_p_values <- matrix(0L, a, a)
+  cent_p_values <- matrix(0L, a, n_measures)
   for (i in seq_len(iter)) {
     if (paired) {
       # For paired data, permute within pairs
@@ -76,22 +87,56 @@ permutation_test <- function(x, y, iter = 1000, paired = FALSE, level = 0.05) {
     trans_perm_y <- combined_trans[perm_idx[perm_y], , ]
     weights_perm_x <- compute_weights(trans_perm_x, type, s = a)
     weights_perm_y <- compute_weights(trans_perm_y, type, s = a)
-    diffs_perm[i, , ] <- weights_perm_x - weights_perm_y
-    p_values <- p_values + 1L * (abs(diffs_perm[i, , ]) >= diffs_true_abs)
+    if (include_centralities) {
+      cent_perm_x <- centralities(weights_perm_x, measures = measures, ...)
+      cent_perm_y <- centralities(weights_perm_y, measures = measures, ...)
+      cent_diffs_perm[i, , ] <- as.matrix(
+        cent_perm_x[, -1L] - cent_perm_y[, -1L]
+      )
+      cent_p_values <- cent_p_values +
+        1L * (abs(cent_diffs_perm[i, , ]) >= cent_diffs_true_abs)
+    }
+    edge_diffs_perm[i, , ] <- weights_perm_x - weights_perm_y
+    edge_p_values <- edge_p_values +
+      1L * (abs(edge_diffs_perm[i, , ]) >= edge_diffs_true_abs)
   }
-  p_values <- p_values / iter
-  sig_diffs <- diffs_true * (p_values < level)
+  edge_p_values <- edge_p_values / iter
+  edge_diffs_sig <- edge_diffs_true * (edge_p_values < level)
   edge_stats <- data.frame(
     edge_name = edge_names,
-    diff_true = c(diffs_true),
-    p_value = c(p_values),
+    diff_true = c(edge_diffs_true),
+    p_value = c(edge_p_values),
     stringsAsFactors = FALSE
   )
-  list(
-    edge_statistics = edge_stats,
-    original_weights_x = weights_x,
-    original_weights_y = weights_y,
-    true_diffs = diffs_true,
-    sig_diffs = sig_diffs
+  out <- list(
+    edges = list(
+      stats = edge_stats,
+      diffs_true = edge_diffs_true,
+      diffs_sig = edge_diffs_sig
+    )
+  )
+  if (include_centralities) {
+    cent_p_values <- cent_p_values / iter
+    cent_diffs_sig <- cent_diffs_true * (cent_p_values < level)
+    cent_stats <- expand.grid(State = cent_x$State, Centrality = measures)
+    cent_stats$diff_true <- as.vector(cent_diffs_true)
+    cent_stats$p_value <- c(cent_p_values)
+    cent_diffs_true <- cbind(
+      data.frame(State = cent_x$State),
+      as.data.frame(cent_diffs_true)
+    )
+    cent_diffs_sig <- cbind(
+      data.frame(State = cent_x$State),
+      as.data.frame(cent_diffs_sig)
+    )
+    out$centralities <- list(
+      stats = cent_stats,
+      diffs_true = cent_diffs_true,
+      diffs_sig = cent_diffs_sig
+    )
+  }
+  structure(
+    out,
+    class = "tna_permutation"
   )
 }
