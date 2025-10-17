9d7ef62709a069b1dd1d67c59ae49b51bb79ad84
santikka <santtu.tikka@jyu.fi>
2025-10-17 21:54:53 +0300

    fix and restore mmm etc.

--- tests/testthat/test-indices.R
+++ tests/testthat/test-indices.R
@@ -1,13 +1,20 @@
-# test_that("sequence indices can be computed", {
-#   expect_error(
-#     sequence_indices(mock_sequence),
-#     NA
-#   )
-# })
-#
-# test_that("favorable states can be specified", {
-#   expect_error(
-#     sequence_indices(mock_sequence, favorable = "A"),
-#     NA
-#   )
-# })
+test_that("sequence indices can be computed", {
+  expect_error(
+    sequence_indices(mock_sequence),
+    NA
+  )
+})
+
+test_that("sequence indices can be computed for tna models", {
+  expect_error(
+    sequence_indices(mock_tna_seq),
+    NA
+  )
+})
+
+test_that("favorable states can be specified", {
+  expect_error(
+    sequence_indices(mock_sequence, favorable = "A"),
+    NA
+  )
+})
